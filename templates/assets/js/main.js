05590923281d00ccde1cc048a781b76397f80674
lyujunpeng <lyujunpeng@outlook.com>
2025-04-01 14:50:58 +0800

    首页临时数据

--- templates/assets/js/main.js
+++ templates/assets/js/main.js
@@ -1,1 +1,12 @@
 console.log("Hi from main.js");
+const clickHamburger = ()=>{
+    const menuElement = document.getElementById("menu");
+    if(menuElement.style.display === "block"){
+        menuElement.style.display = "none";
+    }else{
+        menuElement.style.display = "block";
+    }
+}
+
+const copyrightDomain = document.getElementById("copyright-domain");
+copyrightDomain.innerHTML = window.location.hostname;
